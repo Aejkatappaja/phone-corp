388deba43b9e4986083fca27ccbbc1073424e3f2
Aejkatappaja <joverjlj@gmail.com>
2023-07-20 23:03:33 +0200

    add: routes GET products / GET products:SKU

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -4,20 +4,47 @@ import { IProduct } from "types/types";
 
 const router = express.Router();
 
-router.post("/add-product", async (req: Request, res: Response) => {
+//===========================================================================================================================//
+//==========================================================================================================================//
+//                                     ROUTE CREATED TO INIT MONGODB PRODUCT LIST                                          //
+//========================================================================================================================//
+//=======================================================================================================================//
+
+// router.post("/add-product", async (req: Request, res: Response) => {
+//   try {
+//     const { brand, model, sku, price, quantity }: IProduct = req.body;
+
+//     const newProduct = new Product({
+//       brand,
+//       model,
+//       sku,
+//       price,
+//       quantity,
+//     });
+
+//     await newProduct.save();
+//     res.status(201).json(newProduct);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send(error);
+//   }
+// });
+
+router.get("/products", async (req: Request, res: Response) => {
   try {
-    const { brand, model, sku, price, quantity }: IProduct = req.body;
+    const Products = await Product.find();
+    res.status(200).json({ Products });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
+});
 
-    const newProduct = new Product({
-      brand,
-      model,
-      sku,
-      price,
-      quantity,
-    });
-
-    await newProduct.save();
-    res.status(201).json(newProduct);
+router.get("/product/:sku", async (req: Request, res: Response) => {
+  try {
+    const sku = req.params.sku;
+    const product: IProduct = await Product.findOne({ sku }).exec();
+    res.status(200).json({ product });
   } catch (error) {
     console.error(error);
     res.status(500).send(error);
